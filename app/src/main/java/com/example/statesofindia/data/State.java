5bee0990330f9e4413f77559e33ba1b356617b1c
Adith A Danthi <adith.danthi@gmail.com>
2020-02-03 15:55:05 +0530

    created entity class

---
+++ app/src/main/java/com/example/statesofindia/data/State.java
@@ -0,0 +1,32 @@
+package com.example.statesofindia.data;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "state")
+public class State {
+
+    @PrimaryKey
+    private String mStateName;
+
+    @NonNull
+    @ColumnInfo(name = "capital")
+    private String mCapital;
+
+    public State(String stateName,@NonNull String capital){
+        this.mStateName = stateName;
+        this.mCapital = capital;
+    }
+
+    public String getStateName() {
+        return mStateName;
+    }
+
+    @NonNull
+    public String getCapital() {
+        return mCapital;
+    }
+
+}
