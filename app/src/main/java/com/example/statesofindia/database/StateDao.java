047e50ae432c65a9e9efba8d74a371a5dd3b09e0
Adith A Danthi <adith.danthi@gmail.com>
2020-02-05 12:23:34 +0530

    created dao

---
+++ app/src/main/java/com/example/statesofindia/database/StateDao.java
@@ -0,0 +1,31 @@
+package com.example.statesofindia.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.statesofindia.data.State;
+
+import androidx.paging.DataSource;
+import androidx.room.Update;
+
+@Dao
+public interface StateDao {
+
+    @Insert
+    void insert(State state);
+
+    @Query("SELECT * FROM state ORDER BY mStateName ASC")
+    DataSource.Factory<Integer,State> getAllPagedStates();
+
+    @Query("DELETE FROM state")
+    void deleteAll();
+
+    @Delete
+    void deleteState();
+
+    @Update
+    void updateState(State state);
+
+}
