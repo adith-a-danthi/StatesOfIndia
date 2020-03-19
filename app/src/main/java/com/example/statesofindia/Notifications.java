80a55c7c3df60af2522f349ff419b4173ed58595
Adith A Danthi <adith.danthi@gmail.com>
2020-03-19 11:59:49 +0530

    added daily notifications

--- app/src/main/java/com/example/statesofindia/Notifications.java
+++ app/src/main/java/com/example/statesofindia/Notifications.java
@@ -1,12 +1,19 @@
 package com.example.statesofindia;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
 
 import com.example.statesofindia.data.State;
+import com.example.statesofindia.ui.MainActivity;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
-import static androidx.core.content.ContextCompat.getSystemService;
 
 public class Notifications {
 
@@ -17,6 +24,30 @@ public class Notifications {
     public static void getDailyNotification(Context context, State state){
         mNotifyManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
 
+        //Creating notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel primaryChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Daily Quiz", NotificationManager.IMPORTANCE_LOW);
+            primaryChannel.enableLights(true);
+            primaryChannel.setLightColor(Color.CYAN);
+            mNotifyManager.createNotificationChannel(primaryChannel);
+        }
+
+        //To switch to app when notification is clicked
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Notification to be displayed
+        String notificationTitle = "Daily Quiz Time";
+        String notificationContent = "What is the capital of " + state.getStateName() + " ?";
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(context,PRIMARY_CHANNEL_ID)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationContent)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true);
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
 
--- app/src/main/java/com/example/statesofindia/database/StateRoomDatabase.java
+++ app/src/main/java/com/example/statesofindia/database/StateRoomDatabase.java
@@ -3,7 +3,6 @@ package com.example.statesofindia.database;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.net.Uri;
-import android.telecom.Call;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -19,9 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
